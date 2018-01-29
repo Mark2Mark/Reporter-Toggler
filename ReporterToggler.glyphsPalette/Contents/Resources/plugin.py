47c79d465358998ded0d6f6a066873b3482f1296
Mark2Mark <mirque84@gmail.com>
2018-01-29 15:54:26 +0100

    Put back Georgs Commits
    ... remove custom Checkbox. Georg commits didnt come along in the recent merge conflict, so I put them in manually now. Also my custom Checkbox is removed to clean up the code.

--- ReporterToggler.glyphsPalette/Contents/Resources/plugin.py
+++ ReporterToggler.glyphsPalette/Contents/Resources/plugin.py
@@ -11,175 +11,33 @@
 #
 ###########################################################################################################
 
+import objc
 from GlyphsApp.plugins import *
 from vanilla import *
 from AppKit import NSNotificationCenter, NSSwitchButton, NSShadowlessSquareBezelStyle, NSLeftTextAlignment, NSNoCellMask
 import traceback
 
-# print "Reporter Toggler 2017-11-06"
+print "Reporter Toggler 2018-01-29"
 
 
 def ReporterSort(obj1, obj2):
 	return cmp(obj1.title(), obj2.title())
 
 
-########################################
-########################################
-########################################
-# Classes almost exactly as in the vanilla LIB
-# @ https://github.com/typesupply/vanilla/blob/master/Lib/vanilla/vanillaCheckBox.py
-#
-# Basically I just aim for a change of pos values for style `mini` at `textBoxPosSize` 
-#
-# --> All this code can be removed* to use a classic vanilla style CheckBox, also change
-# checkBox = MFCheckBox( (10, elementHeight*i, ...
-# back to:
-# checkBox = CheckBox( (10, elementHeight*i, ...
-#
-# *) Also can be removed if Typesupply accepts my pull request to change these values
-# [https://github.com/typesupply/vanilla/pull/36]
-
-class _CheckBoxManualBuildButton(Button):
-
-    nsButtonType = NSSwitchButton
-    frameAdjustments = {
-        "regular": (-2, -3, 4, 4),
-        "small": (-3, -7, 5, 4),
-        "mini": (-3, -11, 6, 8),
-        }
-
-    def set(self, value):
-        self._nsObject.setState_(value)
-
-    def get(self):
-        return self._nsObject.state()
-
-    def toggle(self):
-        state = self.get()
-        self.set(not state)
-
-
-class _CheckBoxManualBuildTextButton(Button):
-
-    nsBezelStyle = NSShadowlessSquareBezelStyle
-    frameAdjustments = None
-
-    def __init__(self, posSize, title, callback, sizeStyle):
-        super(_CheckBoxManualBuildTextButton, self).__init__(posSize, title=title, callback=callback)
-        self._nsObject.setBordered_(False)
-        self._setSizeStyle(sizeStyle)
-        self._nsObject.setAlignment_(NSLeftTextAlignment)
-        self._nsObject.cell().setHighlightsBy_(NSNoCellMask)
-
-########################################
-
-
-class MFCheckBox(CheckBox):
-
-	allFrameAdjustments = {
-		"mini": (0, -4, 0, 8),
-		"small": (0, -2, 0, 4),
-		"regular": (0, -2, 0, 4),
-		}
-
-	def __init__(self, posSize, title, callback=None, value=False, sizeStyle="regular"):
-
-		# super(MFCheckBox, self).__init__(posSize, title, callback=None, value=False, sizeStyle=chosenSize)
-
-		self._setupView("NSView", posSize)
-
-		self._callback = callback
-
-		buttonSizes = {
-				"mini": (10, 10),
-				"small": (18, 18),
-				"regular": (22, 22)
-				}
-		left, top, width, height = posSize
-
-		self.frameAdjustments = self.allFrameAdjustments[sizeStyle]
-
-		buttonWidth, buttonHeight = buttonSizes[sizeStyle]
-		buttonLeft, buttonTop = self.frameAdjustments[:2]
-		buttonLeft= abs(buttonLeft)
-		buttonTop = abs(buttonTop)
-
-		# adjust the position of the text button in relation to the check box
-		textBoxPosSize = {
-				# left, top, height
-				## ************************
-				"mini": (12, 5, 12), # Changed by Mark, original: (10, 4, 12)
-				"small": (14, 6, 14), # Changed by Mark, original: (14, 4, 14)
-				## ************************
-				"regular": (16, 3, 17)
-				}
-		textBoxLeft, textBoxTop, textBoxHeight = textBoxPosSize[sizeStyle]
-		textBoxWidth = 0
-
-		self._checkBox = _CheckBoxManualBuildButton((0, 0, buttonWidth, buttonHeight), "", callback=self._buttonHit, sizeStyle=sizeStyle)
-		self._checkBox.set(value)
-
-		self._textButton = _CheckBoxManualBuildTextButton((textBoxLeft, textBoxTop, textBoxWidth, textBoxHeight), title=title, callback=self._buttonHit, sizeStyle=sizeStyle)
-		######
-
-
-		### Added by Mark:
-		try:
-			nsbutton = self._textButton.getNSButton()
-			## Optional Button Styles
-			# nsbutton.setBordered_(1)
-			# nsbutton.setBezelStyle_(1)
-			# nsbutton.setButtonType_(0) # 1 is funky
-			# nsbutton.setAlignment_(0)
-		except:
-			print traceback.format_exc()
-
-########################################
-########################################
-########################################		
-
-
-
-
-
-
-'''
-# Under Construction, not working.
-# Attempt to reduce code above.
-class _CheckBoxManualBuild(VanillaBaseObject):
-	textBoxPosSize = {
-			# left, top, height
-			## ************************
-			"mini": (12, 5, 12), # Changed by Mark, original: (10, 4, 12)
-			"small": (14, 6, 14), # Changed by Mark, original: (14, 4, 14)
-			## ************************
-			"regular": (16, 3, 17)
-			}
-	def __init__(self, posSize, title, callback=None, value=False, sizeStyle="regular"):
-		super(_CheckBoxManualBuild, self).__init__()
-		print "Class B"
-		print self.textBoxPosSize
-class M2FCheckBox(_CheckBoxManualBuild):
-	def __init__(self, posSize, title, callback=None, value=False, sizeStyle="regular"):
-		super(M2FCheckBox, self).__init__(posSize, title, callback=None, value=False, sizeStyle="regular")
-		print "Class C"
-		print self.textBoxPosSize
-'''
-
-
-
-
-
-
-
-
 
 elmtSizes = {  # 14 for mini, 16 for small # in older versions we had small/15; but larger leading is easier to read
-	'mini' : 14,
+	'mini' : 15, # 14
 	'small' : 16,
 }
 chosenSize = 'mini'
 
+
+
+# CheckBox.frameAdjustments = {
+# 		"mini": (0, -4, 0, 8),
+# 		"small": (0, -2, 0, 4),
+# 		"regular": (0, -2, 0, 4),
+# 		}
 
 
 class ReporterToggler (PalettePlugin):
@@ -204,10 +62,8 @@ class ReporterToggler (PalettePlugin):
 					isActive = False
 				# print reporter.classCode()
 				attrName = "CheckBox_%s" % str(i)
-				try:
-					checkBox = MFCheckBox( (10, elementHeight*i, -10, elementHeight), reporter.title(), sizeStyle=chosenSize, value=isActive, callback=self.toggle )
-				except:
-					checkBox = CheckBox( (10, elementHeight*i, -10, elementHeight), reporter.title(), sizeStyle=chosenSize, value=isActive, callback=self.toggle )
+
+				checkBox = CheckBox( (10, elementHeight*i, -10, elementHeight), reporter.title(), sizeStyle=chosenSize, value=isActive, callback=self.toggle )
 				setattr(self.paletteView.group, attrName, checkBox)
 
 			# Set dialog to NSView
@@ -217,10 +73,11 @@ class ReporterToggler (PalettePlugin):
 
 
 	def start(self):
-		# Adding a callback for the 'GSUpdateInterface' event
-		s = objc.selector( self.update, signature="v@:" )
-		NSNotificationCenter.defaultCenter().addObserver_selector_name_object_( self, s, "GSUpdateInterface", None )  # GSDocumentCloseNotification | GSDocumentActivateNotification
+		# Adding a callback for when the visiblity of a reporter changes
+		NSUserDefaults.standardUserDefaults().addObserver_forKeyPath_options_context_(self, "visibleReporters", 0, None)
 
+	def observeValueForKeyPath_ofObject_change_context_(self, keyPath, aObject, change, context):
+		self.update(self)
 
 	def toggle(self, sender):
 		try:
@@ -248,4 +105,5 @@ class ReporterToggler (PalettePlugin):
 
 	def quit(self):
 		# Delete callbacks when Glyphs quits, otherwise it'll crash :( 
-		NSNotificationCenter.defaultCenter().removeObserver_(self)
+		NSUserDefaults.standardUserDefaults().removeObserver_forKeyPath_(self, "visibleReporters")
+
